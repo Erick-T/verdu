8a157660a5f38039065d27af992c416bbe8bd8a0
Erick Torres <erick.93@live.com.ar>
2021-08-10 01:43:37 -0300

    Creando Navbar.

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,16 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'navlinks'
+    }, {
+        title: 'About us',
+        url: '#',
+        cName: 'navlinks'
+    }, {
+        title: 'Services',
+        url: '#',
+        cName: 'navlinks'
+    }
+
+]
