cb330c8d8b7d3ffa1495b26f760ea9bd848b2e7a
Erick Torres <erick.93@live.com.ar>
2021-08-28 12:25:21 -0300

    Cart widget. modificado.

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -2,7 +2,7 @@ import "./CartWidget.css"
 export const CartWidget = () => {
     return (
         <div className="contenedor-carrito">
-            <i className="fas fa-shopping-cart"></i>
+            <i className="fas fa-shopping-cart" id="loguito"></i>
         </div>
     );
 }
